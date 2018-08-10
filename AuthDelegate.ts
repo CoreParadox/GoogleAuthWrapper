7e56f3aaa061e4880a1caf304bc20a32883f7d65
CoreParadox <ClockworkParadox@gmail.com>
2018-08-10 11:27:56 -0500

    Corrected Typings

--- AuthDelegate.ts
+++ AuthDelegate.ts
@@ -1,5 +1,5 @@
 export abstract class AuthDelegate{
     public serialize:(user:any)=>Promise<any>
     public deserialize:(user:any)=>Promise<any>
-    public accessFailed:Promise<void>
+    public accessFailed:(user:any)=>Promise<void>
 }
